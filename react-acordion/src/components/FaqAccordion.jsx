d1c25a942b81f8fd9569b41f111a0503df06fc5e
Nahuel C. <nahuelncejas@gmail.com>
2025-10-02 10:19:42 -0300

    Organice el area de trabajo

---
+++ react-acordion/src/components/FaqAccordion.jsx
@@ -0,0 +1,9 @@
+const FaqAccordion = () => {
+    return (
+        <div style={{ background: "red", height: "100vh", color: "white" }}>
+        <h1>Hola Nahuel 🚀</h1>
+        </div>
+    )
+}
+
+export default FaqAccordion
